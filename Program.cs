a723c960d95701c876ef6332cc5294d70f0c43d4
kazama jin <gordeyshirshov@mail.ru>
2025-05-29 04:07:38 +0700

    linq

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        // The Three Parts of a LINQ Query: 1. Data source.
+        int[] numbers = new int[7] { 0, 1, 2, 3, 4, 5, 6 };
+        // 2. Query creation. 
+        // numQuery is an IEnumerable<int>
+        var numQuery =
+            from num in numbers
+            where (num % 2) == 0
+            select num;
+        // 3. Query execution.
+        foreach (int num in numQuery)
+        {
+            Console.Write("{0,1} ", num);
+        }
+    }
+}
