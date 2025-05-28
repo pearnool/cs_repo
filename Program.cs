f9c8d2ddeb14e9c33bdf863936e0f043c13a4ced
kazama jin <gordeyshirshov@mail.ru>
2025-05-29 02:47:50 +0700

    lab1_done

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static Name.Unit;
+using static Name.Factory;
+using static Name.Tank;
+
+namespace Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создание экземпляров классов
+            var tanks = GetTanks();
+            var factories = GetFactories();
+            var units = GetUnits();
+            // Вывод всех резервуаров с именами цехов и фабрик
+            Console.WriteLine("Список резервуаров:");
+            foreach (var tank in tanks)
+            {
+                var unit = units.FirstOrDefault(u => u.Id == tank.UnitId);
+                var factory = factories.FirstOrDefault(f => f.Id == unit?.Id);
+                Console.WriteLine($"Резервуар: {tank.Name}, Описание: {tank.Description}, " +
+                                    $"Цех: {unit?.Name}, Фабрика: {factory?.Name}");
+            }
+
+            // Вывод общей суммы загрузки всех резервуаров
+            var totalVolume = GetTotalVolume(tanks);
+            Console.WriteLine($"\nОбщая сумма загрузки всех резервуаров: {totalVolume}");
+
+            // Поиск резервуара по имени
+            Console.WriteLine("\nВведите название резервуара для поиска:");
+            string searchName = Console.ReadLine();
+            var foundTank = FindTankByName(tanks, searchName);
+            if (foundTank != null)
+            {
+                var unit = units.FirstOrDefault(u => u.Id == foundTank.UnitId);
+                var factory = factories.FirstOrDefault(f => f.Id == unit?.Id);
+                Console.WriteLine($"Найденный резервуар: {foundTank.Name}, Описание: {foundTank.Description}, " +
+                                    $"Цех: {unit?.Name}, Фабрика: {factory?.Name}");
+            }
+            else
+            {
+                Console.WriteLine("Резервуар не найден.");
+            }
+        }
+
+    }
+}
+
+
+
+
+
---
+++ Unit.cs
@@ -0,0 +1,19 @@
+// Класс цеха
+namespace Name
+{
+    public class Unit
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public static Unit[] GetUnits()
+        {
+            return new Unit[]
+            {
+            new Unit { Id = 1, Name = "Цех 1", Description = "Первый цех" },
+            new Unit { Id = 2, Name = "Цех 2", Description = "Второй цех" }
+            };
+        }
+    }
+}
